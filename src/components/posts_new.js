729427ffee8f045c21d11767359475cd394f28bf
BlakeRedwolf <blakethedead@gmail.com>
2017-09-13 16:19:57 -0700

    Added redux-form to posts_new.js and added <form> and <Field /> elements

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>Title</label>
+        <input 
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+         />
+         <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+         />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({ 
+  form: 'PostsNewForm'
+ }) (PostsNew);
