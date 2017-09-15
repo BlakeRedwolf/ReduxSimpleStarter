4e7e5d39083978ba05faa995ff35cad5fd71ca22
BlakeRedwolf <blakethedead@gmail.com>
2017-09-15 15:22:47 -0700

    Redux Blog is functional and ready for deployment

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from  '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+
   render() {
+    const { post } = this.props;
+    // posts[this.props.match.params.id]; // The post we want to show
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return(
       <div>
-        PostsShow!
+        <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
+          <h3>{post.title}</h3>
+          <h6>Categories: {post.categories}</h6>
+          <p>{post.content}</p>
       </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) { // ownProps is the object that is heading/going to our class based //component PostsShow above, whenever our component is about the be rendered mapStateToProps is called and is passed all //the props that were headed to that PostsShow
+  return { post: posts[ownProps.match.params.id] }; // This will return only the 1 post we are interested in
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow); // Last step for mapStateToProps is to pass it into //our connect function
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,18 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload); // Looks @ state object & if it has a key of the posts id, just drop it (omit it) and return a new object with that post id not present anymore
+      //return _.reject(state, post => post.id === action.payload); // if we a state object of an array it would look something like this
+    case FETCH_POST:
+    //  const post = action.payload.data;
+    //  const newState = { ...state,  };
+    //  newState[post.id] = post;
+    //  return newState;
+
+    return { ...state, [action.payload.data.id]: action.payload.data }; // This is identical to ^ the commented code
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
