88620f8efa4b9946a59d6347810356032904e9b4
BlakeRedwolf <blakethedead@gmail.com>
2017-09-14 22:45:01 -0700

    Form validation finished, implemented programmatic navigation feature, minor css added, started PostsShow component LF145.0.00.01_9.14.17

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=BLAKE1234';
@@ -10,6 +11,16 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(values, callback) { // pass in callback
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values) // after api request success
+    .then(() => callback()); // makes the call to our callback
+
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,30 +1,50 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input 
+          className="form-control"
           type="text"
           {...field.input}
         />
+        <div className="text-help">
+        {touched ? error : ''}
+        </div>
       </div>
     );
   }
 
+  onSubmit(values) {
+    
+    this.props.createPost(values, () => { // Now inside our action creator so we will be navigated back to our new post!
+      this.props.history.push('/'); // This is the route for our programmatic navigation
+    });
+    // calling bind(this) on the <form> element below to make sure we still have access to this === component
+    //console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
-          label="Title"
+          label="Title For Post"
           name="title"
           component={this.renderField}
          />
          <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
          />
          <Field 
@@ -32,11 +52,40 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
          />
+         <button type="submit" className="btn btn-primary">Submit</button>
+         <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
 }
 
-export default reduxForm({ 
+//Define validate helper function and pass to redux form, to be called automatically when the user submits. 
+//(Values) contains the submit data
+function validate(values) {
+  // console.log(values) -> { title: 'asdf', categories: 'asdf', content: 'asdf'}
+  const errors= {};
+
+  // Validate the inputs from 'values;
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content!";
+  }
+  // if the errors object is empty, the form is fine to submit
+  // if errors has any properties, redux form assumes form is invalid
+  return errors;
+}
+// field.meta.error is automatically added to the errors object by redux form
+
+export default reduxForm({
+  validate,
   form: 'PostsNewForm'
- }) (PostsNew);
+ }) (
+   connect(null,{ createPost })(PostsNew) // This is a great example of combining multiple 'connect' like helpers
+); // createPost does render the component and is valid syntax for reduxForm, notice we are passing this in as a 2nd set of ()'s
